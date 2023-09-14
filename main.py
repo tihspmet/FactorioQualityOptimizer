3710867a29f89d685dfdfb0cb38dc0704c20ae67
scottmsul <scottmsul@gmail.com>
2023-09-13 23:37:20 -0400

    fix bug where level 5 was using prod level 4, loop over all quality input/output end products

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def initialize_recipe_matrix(frac_quality):
     for i in range(NUM_QUALITIES-1):
         X[i, NUM_QUALITIES-1] = 10**(i-NUM_QUALITIES+2) * q[i] * p[i]
     
-    X[NUM_QUALITIES-1, NUM_QUALITIES-1] = p[-1]
+    X[NUM_QUALITIES-1, NUM_QUALITIES-1] = 1 + NUM_MODULES * PROD_BONUS
     return X.T
 
 def initialize_recycling_matrix():
@@ -45,7 +45,7 @@ def initialize_recycling_matrix():
     R *= RECYCLING_RATIO
     return R.T
 
-def solve(frac_quality):
+def solve(frac_quality, goal):
     X = initialize_recipe_matrix(frac_quality)
     R = initialize_recycling_matrix()
     # convert to matrix for row reduction
@@ -59,22 +59,40 @@ def solve(frac_quality):
     q1_input[0] = 1
     eqs = np.block([[recipes, q1_input]])
 
-    q5_output = np.zeros(NUM_QUALITIES*2)
-    q5_output[-1] = 1
-
-    result = np.linalg.solve(eqs, q5_output)
+    result = np.linalg.solve(eqs, goal)
     num_input = result[-1]
     return num_input
 
-if __name__ == '__main__':
+def optimize_modules(goal):
     best_frac_quality = None
     best_num_input = 9999999
     possible_frac_qualities = np.array([0, 0.25, 0.5, 0.75, 1])
     for frac_quality in itertools.product(possible_frac_qualities, repeat=4):
         frac_quality = np.array(frac_quality)
-        num_input = solve(frac_quality)
+        num_input = solve(frac_quality, goal)
         if num_input < best_num_input:
             best_num_input = num_input
             best_frac_quality = frac_quality
-    print(f'best quality ratios: {best_frac_quality}')
-    print(f'best Q1 Input per Q% Output: {best_num_input}')
+    return (best_frac_quality, best_num_input)
+
+def loop_over_goals():
+    for i in range(1, NUM_QUALITIES):
+        goal = np.zeros(NUM_QUALITIES*2)
+        goal[i] = 1
+        best_frac_quality, best_num_input = optimize_modules(goal)
+        print(f'Optimizing Q1 Input to Q{i+1} Input: {best_num_input}')
+        print(f'num Q1: {best_num_input}')
+        print(f'quality ratios: {best_frac_quality}')
+        print('')
+
+    for i in range(1, NUM_QUALITIES):
+        goal = np.zeros(NUM_QUALITIES*2)
+        goal[i+NUM_QUALITIES] = 1
+        best_frac_quality, best_num_input = optimize_modules(goal)
+        print(f'Optimizing Q1 Input to Q{i+1} Output: {best_num_input}')
+        print(f'num Q1: {best_num_input}')
+        print(f'quality ratios: {best_frac_quality}')
+        print('')
+
+if __name__ == '__main__':
+    loop_over_goals()
